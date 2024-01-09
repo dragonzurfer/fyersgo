26f7c7a56b1198c8de50d07994935a21998d2702
Shreehari A <dragonzurfer@gmail.com>
2024-01-09 12:03:49 +0530

    Fix v3

--- data_api_client.go
+++ data_api_client.go
@@ -45,7 +45,6 @@ func (c *client) GetHistoricalData(symbol string, resolution api.Resolution, sta
 	} else {
 		queryParam = fmt.Sprintf("%s&range_to=2021-01-02", queryParam)
 	}
-	fmt.Println(c.toUriData(DataV3, HistoricalDataApiUrl, QueryParam, queryParam), nil)
 	if resp, err := c.invoke(utils.GET, c.toUriData(DataV3, HistoricalDataApiUrl, QueryParam, queryParam), nil); err != nil {
 
 		return api.HistoricalData{}, err
